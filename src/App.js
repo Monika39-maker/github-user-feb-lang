61bfe3e97b86772a14e6fbdf1cb65bb2e0180dd4
Monika39-maker <dangolmonika@yahoo.com>
2022-08-12 01:33:45 +0100

    styles added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [userRepos, setUserRepos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUser(e.target.value);
+    fetch(`https://api.github.com/users/${user}/repos`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUserRepos(data);
+      });
+  }
+
+  let repoLanguagesArr = [];
+
+  userRepos.map((repo) => {
+    return repoLanguagesArr.push(repo.language);
+  });
+
+  let languagesUsedCounts = {};
+  repoLanguagesArr.forEach((lang) => {
+    languagesUsedCounts[lang] = (languagesUsedCounts[lang] || 0) + 1;
+  });
+
+  let noOfTimesEachLanguageUsedArr = Object.values(languagesUsedCounts);
+
+  //get the highest value from languagesUsedCounts
+  let max = Math.max(...noOfTimesEachLanguageUsedArr);
+
+  //following code is to create collection of languages
+  // if more than one language has highest count.
+  let favouriteLanguagesArr = [];
+  for (let lang in languagesUsedCounts) {
+    if (languagesUsedCounts[lang] === max) {
+      favouriteLanguagesArr.push(lang);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find Their Favourite Programming Language</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Github Username "
+          onChange={(e) => setUser(e.target.value)}
+        />
+      </form>
+      <div className="result-box">
+        <h3>Favourite language/s:</h3>
+        {favouriteLanguagesArr.map((favLang) => {
+          return (
+            <p key={favLang.id}>
+              {favouriteLanguagesArr.length < 2
+                ? favLang
+                : favouriteLanguagesArr.join(", ")}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 }
